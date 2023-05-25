c6f81c10384284e76d02efde2d8d04a77205ad22
Alberto Francisco <franiglal@gmail.com>
2023-05-26 01:01:00 +0200

    añado constantes de cada tecla para facilitar la configuracion y la activacion de los canales de audio

--- index.js
+++ index.js
@@ -13,6 +13,37 @@ const vlc = new VLC.Client({
 });
 let looping = 0
 
+//constantes de configuracion de teclado midi
+// atem mini
+const CAMARA1_CUT = 32
+const CAMARA2_CUT = 33
+const CAMARA3_CUT = 34
+const CAMARA4_CUT = 36
+const DOWN_KEY = 40
+const VOL1 = 11
+const VOL2 = 12
+const VOL3 = 13
+const VOL4 = 14
+const MIC1 = 15
+const MIC2 = 16
+const MASTER = 10
+const VOL_ON1 = 24
+const VOL_ON2 = 25
+const VOL_ON3 = 26
+const VOL_ON4 = 27 
+const MIC_ON1 = 28
+const MIC_ON2 = 29 
+
+// vlc
+
+const PLAY = 46
+const STOP = 45
+const LOOP = 44
+const FF = 43
+const REW = 42
+const VOL = 18
+
+
 input.getPortCount()
 input.getPortName(0)
 
@@ -32,15 +63,15 @@ input.on('message', async (deltaTime, message ) => {
     // - VLC -
 
     // play pause 
-    if(tecla == 22 && valor == 127) await vlc.togglePlay()
+    if(tecla == PLAY && valor == 127) await vlc.togglePlay()
     // stop 
-    if(tecla == 21 && valor == 127) await vlc.stop()
+    if(tecla == STOP && valor == 127) await vlc.stop()
     // siguiente 
-    if(tecla == 19 && valor == 127) await vlc.next()
+    if(tecla == FF && valor == 127) await vlc.next()
     // atras 
-    if(tecla == 18 && valor == 127)  await vlc.previous()
+    if(tecla == REW && valor == 127)  await vlc.previous()
     // loop  
-    if(tecla == 20 && valor) {
+    if(tecla == LOOP && valor) {
       let control = loop()
       await vlc.setLooping(control)
     }
@@ -48,15 +79,23 @@ input.on('message', async (deltaTime, message ) => {
     // - ATEM MINI PRO -
     
     // camara 1
-    if(tecla == 8 && valor == 127) atemMini.channelInput(1)
+    if(tecla == CAMARA1_CUT && valor == 127) atemMini.channelInput(1)
     // camara 2  
-    if(tecla == 9 && valor == 127) atemMini.channelInput(2)
+    if(tecla == CAMARA2_CUT && valor == 127) atemMini.channelInput(2)
     // camara 3  
-    if(tecla == 10 && valor == 127) atemMini.channelInput(3)
+    if(tecla == CAMARA3_CUT && valor == 127) atemMini.channelInput(3)
     // camara 4
-    if(tecla == 11 && valor == 127) atemMini.channelInput(4)
+    if(tecla == CAMARA4_CUT && valor == 127) atemMini.channelInput(4)
     // downkey
-    if(tecla == 16 && valor == 127) atemMini.downKey()
+    if(tecla == DOWN_KEY && valor == 127) atemMini.downKey()
+    // volumen on off
+    if(tecla = VOL_ON1 && valor == 127) atemMini.audioOnOff(1)
+    if(tecla = VOL_ON2 && valor == 127) atemMini.audioOnOff(2)
+    if(tecla = VOL_ON3 && valor == 127) atemMini.audioOnOff(3)
+    if(tecla = VOL_ON4 && valor == 127) atemMini.audioOnOff(4)
+    if(tecla = MIC_ON1 && valor == 127) atemMini.audioOnOff()
+    if(tecla = MIC_ON2 && valor == 127) atemMini.audioOnOff()
+
 
   } 
 
@@ -66,24 +105,24 @@ input.on('message', async (deltaTime, message ) => {
     // - VLC -
 
     // volumen de 0 a 100
-    if(tecla == 8 ) await vlc.setVolume((rangoFader(valor)))
+    if(tecla == VOL ) await vlc.setVolume((rangoFader(valor)))
 
     // - Atem Mini Pro -
 
     // volumen cam 1
-    if(tecla == 1) atemMini.faderChannel(1,valor)
+    if(tecla == VOL1) atemMini.faderChannel(1,valor)
     // volumen cam 2
-    if(tecla == 2) atemMini.faderChannel(2,valor)
+    if(tecla == VOL2) atemMini.faderChannel(2,valor)
     // volumen cam 3
-    if(tecla == 3) atemMini.faderChannel(3,valor)
+    if(tecla == VOL3) atemMini.faderChannel(3,valor)
     // volumen cam 4
-    if(tecla == 4) atemMini.faderChannel(4,valor)
+    if(tecla == VOL4) atemMini.faderChannel(4,valor)
     // volumen mic/line 1
-    if(tecla == 5) atemMini.faderChannel(4,valor)
+    if(tecla == MIC1) atemMini.faderChannel(4,valor)
     // volumen mic/line 2
-    if(tecla == 6) atemMini.faderChannel(4,valor)
+    if(tecla == MIC2) atemMini.faderChannel(4,valor)
     // volumen maxter
-    if(tecla == 9) atemMini.faderMaxter(valor)
+    if(tecla == MASTER) atemMini.faderMaxter(valor)
 
   }
 })
